b803f314a921cdd5233ae7a5a46d7a1013204522
serenebliss0 <semajayi1234@gmail.com>
2026-04-18 23:49:49 +0100

    Initial commit

---
+++ .pio/build/esp32doit-devkit-v1/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/esp32doit-devkit-v1/FrameworkArduino/cbuf.cpp.o: \
+ C:/Users/Semire/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ C:/Users/Semire/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
---
+++ .pio/build/esp32doit-devkit-v1/lib5c9/BLE/BLESecurity.cpp.d
@@ -0,0 +1,8 @@
+.pio/build/esp32doit-devkit-v1/lib5c9/BLE/BLESecurity.cpp.o: \
+ C:/Users/Semire/.platformio/packages/framework-arduinoespressif32/libraries/BLE/src/BLESecurity.cpp \
+ C:/Users/Semire/.platformio/packages/framework-arduinoespressif32/libraries/BLE/src/BLESecurity.h \
+ C:/Users/Semire/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h \
+ C:/Users/Semire/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/bt/host/bluedroid/api/include/api/esp_gap_ble_api.h \
+ C:/Users/Semire/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_err.h \
+ C:/Users/Semire/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_compiler.h \
+ C:/Users/Semire/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/bt/host/bluedroid/api/include/api/esp_bt_defs.h
